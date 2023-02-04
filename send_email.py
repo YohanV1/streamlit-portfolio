ecae43c9733dfc6dd870fc31305c6269e6dc5c5a
Yohan Vinu <yohanvvinu@gmail.com>
2023-02-04 12:22:56 +0530

    Send email implementation

---
+++ send_email.py
@@ -0,0 +1,13 @@
+import smtplib,ssl
+
+host = "smtp.gmail.com"
+port = 465
+
+username = "yohanvvinu@gmail.com"
+password = "foguexcvzehllzbk"
+reciever = "yohanvvinu@gmail.com"
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(host, port, context=context) as server:
+    server.login(username, password)
+    server.sendmail(username, reciever, message)
