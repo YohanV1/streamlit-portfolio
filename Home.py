b917b579169d649e9f5b389dcd4c5819e0467b2d
Yohan Vinu <yohanvvinu@gmail.com>
2023-02-04 13:50:27 +0530

    Organise layout, and clean up

--- Home.py
+++ Home.py
@@ -3,21 +3,16 @@ import pandas
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+ecol, col1, col2, ecol4 = st.columns([0.1, 0.5, 0.5, 0.2])
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.png", width=300)
 
 with col2:
     st.title("Yohan Vinu")
-    content = """Hi, I'm Yohan. I'll be majoring in Computer Science at SRMIST, KTR."""
+    content = """Hi, I'm Yohan. I'm a CS major at SRMIST, KTR. 
+    Below you can find some of the apps I have built in Python. Feel free to contact me."""
     st.info(content)
-
-content = """
-Below you can find some of the apps I have built in Python. Feel free to contact me.
-"""
-
-st.subheader(content)
 
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
